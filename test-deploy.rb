7e3e79a5786b925d01b3e5d303b14b8de880831b
John Casey <jcasey@redhat.com>
2010-04-26 16:18:33 -0400

    updating test-maintenance scripts.

--- test-deploy.rb
+++ test-deploy.rb
@@ -1,21 +1,45 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'pp'
+
+version = '1.3-SNAPSHOT'
+plugins = ['nexus-audit-plugin']
+
+config_file = File.expand_path( "~/.nxtools/config" )
+puts "attempting to read configuration from: #{config_file}"
+
+if ( File.exists?( config_file ) )
+  config = YAML::load_file( config_file )
+  puts "read configuration:"
+  pp config
+else
+  config = { 'nexus-home'=>"~/apps/nexus", 'nexus-bin'=>"~/apps/nexus/current/bin/jsw/macosx-universal-32", 'use-sudo'=>false, 'maven-goals'=>'clean install' }
+  puts "default configuration:"
+  pp config
+end
 
 # We may need nexus to be available in order to build...
-exit 1 unless system( "mvn clean install" )
+exit 1 unless system( "mvn #{config['maven-goals']}" )
 
-version = "1.1-SNAPSHOT"
-plugins = ["nexus-audit-plugin"]
+stop_cmd = ''
+stop_cmd << 'sudo ' if config['use-sudo']
+stop_cmd << "#{config['nexus-bin']}/nexus stop"
 
-exit 5 unless system( "~/apps/nexus/current/bin/jsw/macosx-universal-32/nexus stop" )
+exit 5 unless system( stop_cmd )
 
 plugins.each do |plugin|
-  exit 2 unless system( "rm -rf ~/apps/nexus/sonatype-work/nexus/plugin-repository/#{plugin}-#{version}" )
+  exit 2 unless system( "rm -rf #{config['nexus-home']}/sonatype-work/nexus/plugin-repository/#{plugin}-#{version}" )
 end
 
 plugins.each do |plugin|
-	exit 3 unless system( "unzip #{plugin}/target/#{plugin}-#{version}-bundle.zip -d ~/apps/nexus/sonatype-work/nexus/plugin-repository" )
+	exit 3 unless system( "unzip #{plugin}/target/#{plugin}-#{version}-bundle.zip -d  #{config['nexus-home']}/sonatype-work/nexus/plugin-repository" )
 end
 
-exit 4 unless system( "~/apps/nexus/current/bin/jsw/macosx-universal-32/nexus start" )
+start_cmd = ''
+start_cmd << 'sudo ' if config['use-sudo']
+start_cmd << "#{config['nexus-bin']}/nexus start"
 
-system( "tail -f ~/apps/nexus/current/logs/wrapper.log" )
+exit 4 unless system( start_cmd )
+
+system( "tail -f  #{config['nexus-home']}/current/logs/wrapper.log" )
--- test-restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-~/apps/nexus/current/bin/jsw/macosx-universal-32/nexus restart || exit 4
-
-tail -f ~/apps/nexus/current/logs/wrapper.log
