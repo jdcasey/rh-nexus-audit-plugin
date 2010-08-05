7e94467ebb97cd3c688c82ad8fa657c971cfdef7
John Casey <jdcasey@commonjava.org>
2010-08-05 11:11:25 -0400

    fix copyright headers

--- nexus-audit-plugin/src/main/java/com/redhat/tools/nexus/audit/statik/AuditIndexHtmlCustomizer.java
+++ nexus-audit-plugin/src/main/java/com/redhat/tools/nexus/audit/statik/AuditIndexHtmlCustomizer.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2010 Red Hat, Inc.
+ *  
+ *  This program is licensed to you under Version 3 only of the GNU
+ *  General Public License as published by the Free Software 
+ *  Foundation. This program is distributed in the hope that it will be 
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ *  PURPOSE.
+ *  
+ *  See the GNU General Public License Version 3 for more details.
+ *  You should have received a copy of the GNU General Public License 
+ *  Version 3 along with this program. 
+ *  
+ *  If not, see http://www.gnu.org/licenses/.
+ */
+
 package com.redhat.tools.nexus.audit.statik;
 
 import org.sonatype.nexus.plugins.rest.AbstractNexusIndexHtmlCustomizer;
